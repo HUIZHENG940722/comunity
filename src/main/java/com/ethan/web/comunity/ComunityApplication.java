1a5447922facb2f7faa589bde8d382f9c184c958
HUIZHENG <zhenghui940722@outlook.com>
2020-01-21 13:32:52 +0800

    项目初始化

---
+++ src/main/java/com/ethan/web/comunity/ComunityApplication.java
@@ -0,0 +1,16 @@
+package com.ethan.web.comunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HUIZHENG
+ */
+@SpringBootApplication
+public class ComunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ethan/web/comunity/ComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.web.comunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
